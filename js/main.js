e8422993aac5ad6708d98f0d56f3f0ee8a039406
coordova <grcordova@gmail.com>
2024-02-01 18:57:09 -0400

    interno.tw - Testimonial Section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ navTriggerBtn.addEventListener("click", () => {
 	navMenu.classList.toggle("nav-is-open");
 	// console.log("clicked");
 });
+
+// swiper
+const swiper = new Swiper(".swiper", {
+	loop: true,
+	/* autoplay: {
+		delay: 3000,
+	}, */
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	// breakpoints
+	slidesPerView: 3,
+	spaceBetween: 20,
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		960: {
+			slidesPerView: 2,
+		},
+		1200: {
+			slidesPerView: 3,
+		},
+	},
+});
